22526175cc8f98c7a16294e8a19e1bf3bee02617
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2026-04-12 09:11:15 +0400

    feat: update theme color settings and enhance color variables for improved design consistency

--- src/config.ts
+++ src/config.ts
@@ -12,8 +12,8 @@ export const siteConfig: SiteConfig = {
 	subtitle: "tadawilifetech",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		hue: 272, // Primary Blue #4F5FA9
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
 		enable: true,

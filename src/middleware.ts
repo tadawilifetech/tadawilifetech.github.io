4dac983a07554a299a77439173e8c333c1004533
Milad Raeisi <6504337+miladsoft@users.noreply.github.com>
2026-04-18 09:16:34 +0400

    feat: refactor middleware to remove admin redirection logic; add custom 404 page with layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,5 @@
 import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware((context, next) => {
-	if (context.url.pathname === "/admin") {
-		return context.redirect("/admin/");
-	}
-
-	const localizedAdminMatch = context.url.pathname.match(/^\/(en|ar|fa)\/admin$/);
-	if (localizedAdminMatch) {
-		return context.redirect(`${context.url.pathname}/`);
-	}
-
+export const onRequest = defineMiddleware((_context, next) => {
 	return next();
 });
